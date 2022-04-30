3eb6ece7ae2ca6da7fe12df69fec451eb59f9266
Christopher Einkorn <chris.einkorn@singlekorn.com>
2022-04-30 14:40:18 -0700

    Test DEV workflow

--- vpc.tf
+++ vpc.tf
@@ -13,9 +13,9 @@ module "vpc-secondary" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${var.project_name}-${var.env}-use2"
-  cidr = "10.172.0.0/20"
+  cidr = "10.174.0.0/20"
 
   azs             = ["usw2-az2", "usw2-az3"]
-  public_subnets  = ["10.172.1.0/26", "10.172.1.64/26"]
-  private_subnets = ["10.172.2.0/23", "10.172.4.0/23"]
+  public_subnets  = ["10.174.1.0/26", "10.174.1.64/26"]
+  private_subnets = ["10.174.2.0/23", "10.174.4.0/23"]
 }
