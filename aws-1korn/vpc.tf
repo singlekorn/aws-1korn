85f802cdd09d80d3590e59ab5de27936a51d0eb6
Christopher Einkorn <chris.einkorn@singlekorn.com>
2023-04-17 17:18:08 -0700

    neogen test vpn (#29)
    * neogen test vpn
    
    * update aws module version

--- aws-1korn/vpc.tf
+++ aws-1korn/vpc.tf
@@ -1,13 +1,28 @@
-# module "vpc-usw2" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc-usw2" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = "${var.project_name}-${var.env}"
-#   cidr = "10.170.0.0/20"
+  name = "${var.project_name}-${var.env}"
+  cidr = "10.170.0.0/20"
 
-#   azs             = ["usw2-az2", "usw2-az3"]
-#   public_subnets  = ["10.170.1.0/26", "10.170.1.64/26"]
-#   private_subnets = ["10.170.2.0/23", "10.170.4.0/23"]
-# }
+  azs             = ["usw2-az2", "usw2-az3"]
+  public_subnets  = ["10.170.1.0/26", "10.170.1.64/26"]
+  private_subnets = ["10.170.2.0/23", "10.170.4.0/23"]
+
+  enable_vpn_gateway = true
+
+  customer_gateways = {
+    IP1 = {
+      bgp_asn     = 65111
+      ip_address  = "216.157.208.175"
+      device_name = "lan-lab-fw0001-IP1"
+    },
+    IP2 = {
+      bgp_asn    = 65111
+      ip_address  = "216.157.208.176"
+      device_name = "lan-lab-fw0001-IP2"
+    }
+  }
+}
 
 # module "vpc-use2" {
 #   source = "terraform-aws-modules/vpc/aws"
