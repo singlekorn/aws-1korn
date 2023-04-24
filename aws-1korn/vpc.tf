7b81cca5e4cbd8f91e3bc6c0c1eefc305a1f4fb0
Christopher Einkorn <chris.einkorn@singlekorn.com>
2023-04-24 10:13:59 -0700

    Fix

--- aws-1korn/vpc.tf
+++ aws-1korn/vpc.tf
@@ -24,16 +24,14 @@
 #   }
 # }
 
-# # module "vpc-use2" {
-# #   source = "terraform-aws-modules/vpc/aws"
-# #   providers = {
-# #     aws = aws.secondary
-# #    }
-
-# #   name = "${var.project_name}-${var.env}"
-# #   cidr = "10.174.0.0/20"
-
-# #   azs             = ["use2-az2", "use2-az3"]
-# #   public_subnets  = ["10.174.1.0/26", "10.174.1.64/26"]
-# #   private_subnets = ["10.174.2.0/23", "10.174.4.0/23"]
-# # }
+# module "vpc-use2" {
+#   source = "terraform-aws-modules/vpc/aws"
+#   providers = {
+#     aws = aws.secondary
+#    
+#   name = "${var.project_name}-${var.env}"
+#   cidr = "10.174.0.0/20
+#   azs             = ["use2-az2", "use2-az3"]
+#   public_subnets  = ["10.174.1.0/26", "10.174.1.64/26"]
+#   private_subnets = ["10.174.2.0/23", "10.174.4.0/23"]
+# }
